cfb27f36cc74f0b746e00867735648ffb7910460
igor-gc <igor.gabriel7@icloud.com>
2026-09-08 19:41:38 -0300

    feat: add while do-while and for loops

---
+++ src/br/com/igorgc/controlflow/LoopsWhileFor.java
@@ -0,0 +1,18 @@
+package br.com.igorgc.controlflow;
+
+public class LoopsWhileFor {
+    public static void main(String[] args) {
+        // while, do while, for
+        int count = 0;
+        while (count < 10) {
+            System.out.println(++count);
+        }
+        count = 0;
+        do {
+            System.out.println("Inside the do-while " + ++count);
+        } while (count < 10);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("For " + i);
+        }
+    }
+}
