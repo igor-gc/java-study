5d15a23f3eb39713ab450793230054a6d1fd346a
igor-gc <igor.gabriel7@icloud.com>
2026-09-09 08:20:49 -0300

    feat: add array

---
+++ src/br/com/igorgc/arrays/Array.java
@@ -0,0 +1,19 @@
+package br.com.igorgc.arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] ages = new int[5];
+
+        ages[0] = 21;
+        ages[1] = 15;
+        ages[2] = 11;
+        ages[3] = 30;
+        ages[4] = 40;
+
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+        System.out.println(ages[3]);
+        System.out.println(ages[4]);
+    }
+}
