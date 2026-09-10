83473e756fb6041bc63b511edbb1080e5633a94c
igor-gc <igor.gabriel7@icloud.com>
2026-09-10 15:53:18 -0300

    feat: add oop constructors

---
+++ src/br/com/igorgc/oop/constructors/test/SeriesTest01.java
@@ -0,0 +1,11 @@
+package br.com.igorgc.oop.constructors.test;
+
+import br.com.igorgc.oop.constructors.domain.Series;
+
+public class SeriesTest01 {
+    public static void main(String[] args) {
+        Series series = new Series("Breaking Bad", "TV", 62, "Crime");
+
+        series.print();
+    }
+}
