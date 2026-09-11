19cdb5674b648de45cb61db5e4bde53f566bc3fa
igor-gc <igor.gabriel7@icloud.com>
2026-09-11 10:15:42 -0300

    feat: add oop association arrays with objects

---
+++ src/br/com/igorgc/oop/association/test/PlayerTest01.java
@@ -0,0 +1,20 @@
+package br.com.igorgc.oop.association.test;
+
+import br.com.igorgc.oop.association.domain.Player;
+
+public class PlayerTest01 {
+    public static void main(String[] args) {
+        Player player1 = new Player("Pelé");
+        Player player2 = new Player("Romário");
+        Player player3 = new Player("Cafu");
+        Player[] players = {player1, player2, player3};
+
+        for (Player player : players) {
+            player.print();
+            
+        }
+
+    }
+
+
+}
