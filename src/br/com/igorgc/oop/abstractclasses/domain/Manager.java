748be45e61e65c5f7fdb7ae9b8133329f3e0f58d
igor-gc <igor.gabriel7@icloud.com>
2026-09-17 07:23:15 -0300

    feat: add oop abstract classes

---
+++ src/br/com/igorgc/oop/abstractclasses/domain/Manager.java
@@ -0,0 +1,16 @@
+package br.com.igorgc.oop.abstractclasses.domain;
+
+public class Manager extends Employee {
+
+    public Manager(String name, double salary) {
+        super(name, salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ src/br/com/igorgc/oop/abstractclasses/test/EmployeeTest01.java
@@ -0,0 +1,15 @@
+package br.com.igorgc.oop.abstractclasses.test;
+
+import br.com.igorgc.oop.abstractclasses.domain.Developer;
+import br.com.igorgc.oop.abstractclasses.domain.Manager;
+
+public class EmployeeTest01 {
+
+    public static void main(String[] args) {
+        Manager manager = new Manager("Alysson", 5000);
+        Developer developer = new Developer("Jake", 12000);
+
+        System.out.println(manager);
+        System.out.println(developer);
+    }
+}
