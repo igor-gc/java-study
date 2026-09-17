21ef2e407a7f9bf3dcb2f9a5ce16860ed809ac91
igor-gc <igor.gabriel7@icloud.com>
2026-09-17 07:34:42 -0300

    feat: add oop abstract classea - methods

--- src/br/com/igorgc/oop/abstractclasses/domain/Employee.java
+++ src/br/com/igorgc/oop/abstractclasses/domain/Employee.java
@@ -7,5 +7,16 @@ public abstract class Employee {
     public Employee(String name, double salary) {
         this.name = name;
         this.salary = salary;
+        calculateBonus();
+    }
+
+    public abstract void calculateBonus();
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
     }
 }
