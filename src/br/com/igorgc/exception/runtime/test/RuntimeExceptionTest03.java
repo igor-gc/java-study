3ac26342c63d15a55c1f4f54ab637fcd62766c9f
igor-gc <igor.gabriel7@icloud.com>
2026-09-18 11:24:11 -0300

    feat: add exceptions - finally block

---
+++ src/br/com/igorgc/exception/runtime/test/RuntimeExceptionTest03.java
@@ -0,0 +1,18 @@
+package br.com.igorgc.exception.runtime.test;
+
+public class RuntimeExceptionTest03 {
+    public static void main(String[] args) {
+        openConnection();
+    }
+
+    private static void openConnection() {
+        try {
+            System.out.println("Opening connection");
+            System.out.println("Writing to the database");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Closing connection");
+        }
+    }
+}
