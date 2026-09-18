fd52f0a5dafdb7a03a4325b5b2e01a16f7a63489
igor-gc <igor.gabriel7@icloud.com>
2026-09-18 11:38:08 -0300

    feat: add exceptions - collapsed multi catch

--- src/br/com/igorgc/exception/runtime/test/RuntimeExceptionTest04.java
+++ src/br/com/igorgc/exception/runtime/test/RuntimeExceptionTest04.java
@@ -1,34 +1,27 @@
 package br.com.igorgc.exception.runtime.test;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try {
             throw new RuntimeException();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Inside ArrayIndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Inside IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Inside IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Inside ArithmeticException");
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Inside ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException"
+            );
         } catch (RuntimeException e) {
             System.out.println("Inside RuntimeException");
         }
 
         try {
             maybeThrowException();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
     }
 
     private static void maybeThrowException()
-            throws SQLException, FileNotFoundException {
+            throws SQLException, IOException {
     }
 }
