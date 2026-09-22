17b7eac799582ae91630e678aaec2ba1db2303ac
igor-gc <igor.gabriel7@icloud.com>
2026-09-22 18:29:59 -0300

    feat: add utility classes - DateTimeFormatter

---
+++ src/br/com/igorgc/utilityclasses/formatting/test/DateTimeFormatterTest01.java
@@ -0,0 +1,61 @@
+package br.com.igorgc.utilityclasses.formatting.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateTimeFormatterTest01 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+
+        String s1 = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String s2 = date.format(DateTimeFormatter.ISO_DATE);
+        String s3 = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        LocalDate parse1 =
+                LocalDate.parse("20260922", DateTimeFormatter.BASIC_ISO_DATE);
+        LocalDate parse2 =
+                LocalDate.parse("2026-09-22+05:00", DateTimeFormatter.ISO_DATE);
+        LocalDate parse3 =
+                LocalDate.parse("2026-09-22", DateTimeFormatter.ISO_LOCAL_DATE);
+
+        System.out.println(parse1);
+        System.out.println(parse2);
+        System.out.println(parse3);
+
+        LocalDateTime now = LocalDateTime.now();
+
+        String s4 = now.format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(s4);
+
+        LocalDateTime parse4 =
+                LocalDateTime.parse("2026-09-22T19:01:48.4109373");
+        System.out.println(parse4);
+
+        // dd/MM/yyyy, MM/dd/yyyy, yyyy/MM/dd
+        DateTimeFormatter formatterBR =
+                DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        String formatBR = LocalDate.now().format(formatterBR);
+        System.out.println(formatBR);
+
+        LocalDate parseBR =
+                LocalDate.parse("22/09/2026", formatterBR);
+        System.out.println(parseBR);
+
+        DateTimeFormatter formatterIT =
+                DateTimeFormatter.ofPattern("dd.MMMM.yyyy", Locale.ITALY);
+
+        String formatIT = LocalDate.now().format(formatterIT);
+        System.out.println(formatIT);
+
+        LocalDate parseIT =
+                LocalDate.parse("22.settembre.2026", formatterIT);
+        System.out.println(parseIT);
+    }
+}
