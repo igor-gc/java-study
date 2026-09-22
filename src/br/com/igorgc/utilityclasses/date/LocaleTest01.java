dcc0224c8c25a6f8059a4096af40423b52c957b8
igor-gc <igor.gabriel7@icloud.com>
2026-09-22 17:21:15 -0300

    feat: add utility classes - date internationalization with locale

---
+++ src/br/com/igorgc/utilityclasses/date/LocaleTest01.java
@@ -0,0 +1,38 @@
+package br.com.igorgc.utilityclasses.date;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        Locale localeItaly = new Locale("it", "IT");
+        Locale localeSwitzerland = new Locale("it", "CH");
+        Locale localeIndia = new Locale("hi", "IN");
+        Locale localeJapan = new Locale("ja", "JP");
+        Locale localeNetherlands = new Locale("nl", "NL");
+
+        Calendar calendar = Calendar.getInstance();
+
+        DateFormat dateFormatItaly =
+                DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat dateFormatSwitzerland =
+                DateFormat.getDateInstance(DateFormat.FULL, localeSwitzerland);
+        DateFormat dateFormatIndia =
+                DateFormat.getDateInstance(DateFormat.FULL, localeIndia);
+        DateFormat dateFormatJapan =
+                DateFormat.getDateInstance(DateFormat.FULL, localeJapan);
+        DateFormat dateFormatNetherlands =
+                DateFormat.getDateInstance(DateFormat.FULL, localeNetherlands);
+
+        System.out.println("Italy: " + dateFormatItaly.format(calendar.getTime()));
+        System.out.println("Switzerland: " + dateFormatSwitzerland.format(calendar.getTime()));
+        System.out.println("India: " + dateFormatIndia.format(calendar.getTime()));
+        System.out.println("Japan: " + dateFormatJapan.format(calendar.getTime()));
+        System.out.println("Netherlands: " + dateFormatNetherlands.format(calendar.getTime()));
+
+        System.out.println(localeItaly.getDisplayCountry(localeJapan));
+        System.out.println(localeSwitzerland.getDisplayCountry(localeNetherlands));
+        System.out.println(localeItaly.getDisplayLanguage(localeJapan));
+    }
+}
