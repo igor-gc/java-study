b3fb5cce187c822e6444ae16a9be42a3506f5482
igor-gc <igor.gabriel7@icloud.com>
2026-09-23 07:37:20 -0300

    feat: add utility classes - regex - Pattern and Matcher

---
+++ src/br/com/igorgc/utilityclasses/regex/test/PatternMatcherTest01.java
@@ -0,0 +1,24 @@
+package br.com.igorgc.utilityclasses.regex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest01 {
+    public static void main(String[] args) {
+        String regex = "aba";
+//        String text = "abaaba";
+        String text = "abababa";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        System.out.println("Text:  " + text);
+        System.out.println("Index: 0123456789");
+        System.out.println("Regex: " + regex);
+        System.out.println("Positions found:");
+
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " ");
+        }
+    }
+}
