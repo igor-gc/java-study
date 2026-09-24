332a2fc16ef0d8cfb5900e25224b412bcd71e11a
igor-gc <igor.gabriel7@icloud.com>
2026-09-24 12:46:46 -0300

    feat: add collections - Sorting list - Comparator

--- src/br/com/igorgc/collections/test/SeriesSortTest01.java
+++ src/br/com/igorgc/collections/test/SeriesSortTest01.java
@@ -4,7 +4,16 @@ import br.com.igorgc.collections.domain.Series;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class SeriesByIdComparator implements Comparator<Series> {
+
+    @Override
+    public int compare(Series series1, Series series2) {
+        return series1.getId().compareTo(series2.getId());
+    }
+}
 
 public class SeriesSortTest01 {
 
@@ -22,6 +31,16 @@ public class SeriesSortTest01 {
         }
 
         Collections.sort(series);
+
+        System.out.println("-------------");
+
+        for (Series serie : series) {
+            System.out.println(serie);
+        }
+
+        // Collections.sort(series, new SeriesByIdComparator());
+
+        series.sort(new SeriesByIdComparator());
 
         System.out.println("-------------");
 
