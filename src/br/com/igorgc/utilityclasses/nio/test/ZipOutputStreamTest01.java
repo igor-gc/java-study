47595ca9900997bdd977022ebe7c860e459ae16b
igor-gc <igor.gabriel7@icloud.com>
2026-09-24 11:06:46 -0300

    feat: add utility classes -NIO - ZipOutputStream

---
+++ src/br/com/igorgc/utilityclasses/nio/test/ZipOutputStreamTest01.java
@@ -0,0 +1,42 @@
+package br.com.igorgc.utilityclasses.nio.test;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipOutputStreamTest01 {
+
+    public static void main(String[] args) {
+        Path zipFile = Paths.get("folder/archive.zip");
+        Path filesToZip = Paths.get("folder/subfolder");
+
+        zip(zipFile, filesToZip);
+    }
+
+    private static void zip(Path zipFile, Path filesToZip) {
+        try (
+                ZipOutputStream zipStream =
+                        new ZipOutputStream(Files.newOutputStream(zipFile));
+                DirectoryStream<Path> directoryStream =
+                        Files.newDirectoryStream(filesToZip)
+        ) {
+            for (Path file : directoryStream) {
+                ZipEntry zipEntry =
+                        new ZipEntry(file.getFileName().toString());
+
+                zipStream.putNextEntry(zipEntry);
+                Files.copy(file, zipStream);
+                zipStream.closeEntry();
+            }
+
+            System.out.println("ZIP file created successfully");
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
