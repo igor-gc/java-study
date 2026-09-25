32d8b18585029eaa1cef02af383149eb67b95814
igor-gc <igor.gabriel7@icloud.com>
2026-09-25 18:59:48 -0300

    feat: add lambdas - Consumer

---
+++ src/br/com/igorgc/lambdas/test/LambdaTest01.java
@@ -0,0 +1,20 @@
+package br.com.igorgc.lambdas.test;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class LambdaTest01 {
+    public static void main(String[] args) {
+        List<String> names = List.of("Alice", "Bob", "Charlie", "David");
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        forEach(names, name -> System.out.println(name));
+        forEach(numbers, number -> System.out.println(number));
+    }
+
+    private static <T> void forEach(List<T> list, Consumer<T> consumer) {
+        for (T element : list) {
+            consumer.accept(element);
+        }
+    }
+}
--- src/br/com/igorgc/parameterization/interfaces/CarPredicate.java
+++ src/br/com/igorgc/parameterization/interfaces/CarPredicate.java
@@ -2,6 +2,11 @@ package br.com.igorgc.parameterization.interfaces;
 
 import br.com.igorgc.parameterization.domain.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
+    // Anonymous classes, functions, concise syntax
     boolean test(Car car);
+
+    // (parameter) -> <expression>
+    // (Car car) -> car.getColor().equals("green");
 }
